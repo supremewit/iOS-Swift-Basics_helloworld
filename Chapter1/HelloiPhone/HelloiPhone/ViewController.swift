67aa2dd8eb1ef4142c1b6e2929298f0f77b04d5b
supremewit <31872381+supremewit@users.noreply.github.com>
2017-10-30 23:09:45 +0900

    Chapter 1-4

--- Chapter1/HelloiPhone/HelloiPhone/ViewController.swift
+++ Chapter1/HelloiPhone/HelloiPhone/ViewController.swift
@@ -2,28 +2,31 @@
 //  ViewController.swift
 //  HelloiPhone
 //
-//  Created by Lingostar on 2015. 11. 2..
-//  Copyright © 2015년 CodersHigh. All rights reserved.
+//  Created by Segaon_PC on 2017. 10. 30..
+//  Copyright © 2017년 CodersHigh. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var myLabel: UILabel!
 
+    @IBOutlet weak var thisLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func change(sender: AnyObject) {
-        myLabel.text = "Hello World"
+    
+    @IBAction func change(_ sender: Any) {
+        
+        thisLabel.text = "Hello iPhone"
     }
+
+
 }
 
